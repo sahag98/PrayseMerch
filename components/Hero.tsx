9eae2a85d686838a69f6a78de26a19f92427d8b5
Sahak Arzoumanian <arzsahag@gmail.com>
2024-04-27 16:44:18 -0700

    removed hero animations for better seo

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,39 +1,27 @@
-"use client";
-
-import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
-
 import React from "react";
 import { Button } from "./ui/button";
 import Image from "next/image";
-import { Link } from "next-view-transitions";
+import Link from "next/link";
 
 const Hero = () => {
-  useGSAP(() => {
-    gsap.to("#hero-img", { opacity: 1, x: 0, duration: 0.5 });
-    gsap.to("#stagger", { opacity: 1, duration: 0.5, stagger: 0.2 });
-  }, []);
   return (
     <div
       suppressHydrationWarning={true}
       className="flex w-full lg:flex-row flex-col bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary/15 via-background to-background lg:gap-0 gap-6 h-screen justify-center lg:items-center items-start mt-20 lg:mt-0 relative lg:justify-center"
     >
       <section className="flex flex-col">
-        <h1 id="stagger" className="lg:text-7xl opacity-0 text-5xl font-bold ">
+        <h1 id="stagger" className="lg:text-7xl  text-5xl font-bold ">
           Prayse Merch
         </h1>
 
-        <p
-          className="opacity-0 text-foreground/55 lg:w-2/3 w-full"
-          id="stagger"
-        >
+        <p className=" text-foreground/55 lg:w-2/3 w-full" id="stagger">
           Perfect for everyday wear, reminding us of the power of prayer and
           praise in our walk with God.
         </p>
         <Link className="lg:w-1/3 md:w-1/2 w-full" href="#chip">
           <Button
             id="stagger"
-            className="w-full mt-3 opacity-0 text-base bg-primary font-bold"
+            className="w-full mt-3  text-base bg-primary font-bold"
           >
             Shop Now
           </Button>
@@ -41,7 +29,7 @@ const Hero = () => {
       </section>
       <section
         id="hero-img"
-        className="w-full opacity-0 translate-x-[1000px] overflow-hidden grid grid-cols-2 place-items-center h-fit lg:w-fit gap-3 place-content-center rounded-lg"
+        className="w-full grid grid-cols-2 place-items-center h-fit lg:w-fit gap-3 place-content-center rounded-lg"
       >
         <Image
           src={"/hero-shirt.png"}
